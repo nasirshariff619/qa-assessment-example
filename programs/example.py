a4c2d7bb6b6ec6d74132645ef5200ced06123902
Victoria <32303351+VSacr1@users.noreply.github.com>
2022-04-01 09:01:15 +0100

    Emptied

--- programs/example.py
+++ programs/example.py
@@ -13,10 +13,5 @@
 
     # What was the name of the function we have seen which changes the case of a string?  Use your CLI to access the Python documentation and get help(str).
     
-def endsPy(input):
-    input = input.upper()
-    if input[-2:] == "PY":
-        return True
-    else: 
-        return False
+
     
